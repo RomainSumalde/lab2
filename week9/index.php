e390830886d5c0af24c47d2ecd16362033572f57
RomainSumalde <rcsumalde@student.apc.edu.ph>
2023-02-05 10:58:33 +0800

    Week 9 Button

--- week9/index.php
+++ week9/index.php
@@ -40,27 +40,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       $emailErr = "Invalid email format";
     }
   }
-    
-  if (empty($_POST["website"])) {
-    $website = "";
-  } else {
-    $website = test_input($_POST["website"]);
-    // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-    if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$website)) {
-      $websiteErr = "Invalid URL";
-    }
-  }
 
   if (empty($_POST["comment"])) {
     $comment = "";
   } else {
     $comment = test_input($_POST["comment"]);
-  }
-
-  if (empty($_POST["gender"])) {
-    $genderErr = "Gender is required";
-  } else {
-    $gender = test_input($_POST["gender"]);
   }
 }
 
@@ -217,18 +201,10 @@ function test_input($data) {
                             E-mail: <input type="text" name="email" value="<?php echo $email;?>">
                             <span class="error">* <?php echo $emailErr;?></span>
                             <br><br>
-                            Website: <input type="text" name="website" value="<?php echo $website;?>">
-                            <span class="error"><?php echo $websiteErr;?></span>
-                            <br><br>
                             Comment: <textarea name="comment" rows="5" cols="40"><?php echo $comment;?></textarea>
                             <br><br>
-                            Gender:
-                            <input type="radio" name="gender" <?php if (isset($gender) && $gender=="female") echo "checked";?> value="female">Female
-                            <input type="radio" name="gender" <?php if (isset($gender) && $gender=="male") echo "checked";?> value="male">Male
-                            <input type="radio" name="gender" <?php if (isset($gender) && $gender=="other") echo "checked";?> value="other">Other  
-                            <span class="error">* <?php echo $genderErr;?></span>
-                            <br><br>
                             <input type="submit" name="submit" value="Submit">  
+                            <button type="submit" class="btn btn2">Submit</button>
                         </form>
 
                         <?php
@@ -237,11 +213,7 @@ function test_input($data) {
                             echo "<br>";
                             echo $email;
                             echo "<br>";
-                            echo $website;
-                            echo "<br>";
                             echo $comment;
-                            echo "<br>";
-                            echo $gender;
                         ?>
                         <span id="msg"></span>
                     </div>
